99e12871678bf11446fa8d3ef8405f177ada919a
Eliza Voets <25413@ma-web.nl>
2022-11-29 12:30:13 +0100

    house

---
+++ app.js
@@ -0,0 +1,14 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello, world!");
+        //code
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+const catcafe = {naam:"cat cafe simulator", versienummer:"0.9 double", versiedatum: new Date(2022, 10, 22, 13, 02), autheur: "Eliza Voets", copyright:"creative comments", distributeur: "unknown", darkmode:true};
+console.log(catcafe)
